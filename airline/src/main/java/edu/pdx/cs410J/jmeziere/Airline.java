3629b223118321082ee9c73ec1cb617aa8beadf7
Jacob Meziere <jmeziere@pdx.edu>
2022-01-19 14:15:52 -0800

    Updated javadoc comments for Airline and Flight classes

--- airline/src/main/java/edu/pdx/cs410J/jmeziere/Airline.java
+++ airline/src/main/java/edu/pdx/cs410J/jmeziere/Airline.java
@@ -5,26 +5,46 @@ import edu.pdx.cs410J.AbstractAirline;
 import java.util.Collection;
 import java.util.Vector;
 
+/**
+ * This class represents an <code>Airline</code> and its <code>Flight</code>/s.
+ */
 public class Airline extends AbstractAirline<Flight> {
   private final String name;
   private Collection<Flight> flights;
 
+  /**
+   * Creates a new <code>Airline</code>
+   * @param name
+   *        The name of the airline.
+   */
   public Airline(String name) {
     this.name = name;
     this.flights = new Vector<Flight>();
   }
 
+  /**
+   * Retrieves the <code>Airline</code> name.
+   * @return name
+   */
   @Override
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Adds a new flight under the <code>Airline</code>.
+   * @param flight
+   *        The <code>Flight</code> object to be added to the <code>Airline</code>.
+   */
   @Override
   public void addFlight(Flight flight) {
     this.flights.add(flight);
-    //throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
+  /**
+   * Retrieves the list of <code>Flight</code>s under the <code>Airline</code>.
+   * @return flights
+   */
   @Override
   public Collection<Flight> getFlights() {
     return this.flights;
--- airline/src/main/java/edu/pdx/cs410J/jmeziere/Flight.java
+++ airline/src/main/java/edu/pdx/cs410J/jmeziere/Flight.java
@@ -2,6 +2,9 @@ package edu.pdx.cs410J.jmeziere;
 
 import edu.pdx.cs410J.AbstractFlight;
 
+/**
+ * This class represents a <code>Flight</code>.
+ */
 public class Flight extends AbstractFlight {
   private int flightNumber;
   private String src;
@@ -9,6 +12,21 @@ public class Flight extends AbstractFlight {
   private String dest;
   private String arrive;
 
+  /**
+   * Creates a new <code>Flight</code>.
+   * @param flightNumber
+   *        The number of the flight, up to 6 digits long.
+   * @param src
+   *        The 3-letter airport code for where the flight starts.
+   * @param depart
+   *        The date and 24-hour time of the flight's departure
+   *        as "mm/dd/yyyy hh:mm".
+   * @param dest
+   *        The 3-letter airport code for where the flight ends.
+   * @param arrive
+   *        The date and 24-hour time of the flight's arrival
+   *        as "mm/dd/yyyy hh:mm".
+   */
   public Flight(int flightNumber, String src, String depart, String dest, String arrive) {
     this.flightNumber = flightNumber;
     this.src = src;
@@ -17,30 +35,49 @@ public class Flight extends AbstractFlight {
     this.arrive = arrive;
   }
 
+  /**
+   * Retrieves <code>Flight</code> number.
+   * @return flightNumber
+   */
   @Override
   public int getNumber() {
     return this.flightNumber;
-    //return 42;
   }
 
+  /**
+   * Retrieves <code>Flight</code> source.
+   * @return src
+   */
   @Override
   public String getSource() {
     return this.src;
     //throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
+  /**
+   * Retrieves <code>Flight</code> departure date and time as a <code>String</code>.
+   * @return depart
+   */
   @Override
   public String getDepartureString() {
     return this.depart;
     //throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
+  /**
+   * Retrieves <code>Flight</code> destination.
+   * @return dest
+   */
   @Override
   public String getDestination() {
     return this.dest;
     //throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
+  /**
+   * Retrieves <code>Flight</code> arrival date and time as a <code>String</code>.
+   * @return arrive
+   */
   @Override
   public String getArrivalString() {
     return this.arrive;
